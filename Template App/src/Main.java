5aa96cc3d15c1b495af302f63d38fa8613d0ac06
Azolyx <108442846+Azolyx@users.noreply.github.com>
2022-12-13 13:49:11 -0600

    Update Main.java

--- Template App/src/Main.java
+++ Template App/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public AppManager appManager = new AppManager();
 
     Main() {
-        appManager.init();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        appManager.init("Template App", screenSize.width, screenSize.height, 3);
     }
 
     public static void main(String[] args) { new Main(); }
